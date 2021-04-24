e0a12608e61e7a39606455acbf075a406ff14a26
Sebastien Just <sebastien.just@seij.net>
2021-04-24 12:24:01 +0200

    Code format

--- src/Table.tsx
+++ src/Table.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { GridProvider, useGrid, useGridItem, useGridItemProperty } from "./GridContext";
 import { GridProps } from "./types";
 
-
 enum LoadingState {
   init,
   pending,
